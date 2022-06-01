f64969e1d3e7cf7c178c10909c5c0e5da1490c26
rere2133 <rere2133@users.noreply.github.com>
2022-06-02 01:16:56 +0800

    dev & prod 錯誤錯誤

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var app = express();
 
 require("./connections");
 const handleError = require("./services/handleError");
+const res = require("express/lib/response");
 
 process.on("uncaughtException", (err) => {
   console.log("Uncaught Exception");
@@ -34,13 +35,40 @@ app.use((req, res, next) => {
     message: "找不到頁面！",
   });
 });
+
+const resDevError = (err, res) => {
+  res.status(err.statusCode).json({
+    message: err.message,
+    err: err,
+    stack: err.stack,
+  });
+};
+const resProdError = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      message: "發生錯誤，請稍後再試",
+    });
+  }
+};
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-  res.status(err.statusCode).json({
-    status: "error",
-    message: err.message,
-  });
+  //dev
+  if (process.env.NODE_ENV == "dev") {
+    return resDevError(err, res);
+  }
+  //prod
+  if (err.name == "ValidationError") {
+    err.message = "欄位未填寫正確，請重新輸入";
+    err.isOperational = true;
+    return resProdError(err, res);
+  }
+  resProdError(err, res);
 });
+
 process.on("unhandledRejection", (err, promise) => {
   console.log("未捕捉到的Rejection:", promise);
   console.log("原因:" + err);
